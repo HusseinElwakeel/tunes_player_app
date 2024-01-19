467f66aab28f73bfaca5d1b7294dfc1f7f6cbdd0
Hussein <husseinelwakeel765@gmail.com>
2024-01-19 22:43:21 +0200

    Change colors and finish app

---
+++ lib/Constant/ContainerColors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:tunes_player_app/models/ContainerModel.dart';
+
+List<ContainerModel> container = [
+  ContainerModel(color: Color(0xff5f7a83), sound: 'sounds/note1.wav'),
+  ContainerModel(color: Color(0xff6f8b8f), sound: 'sounds/note2.wav'),
+  ContainerModel(color: Color(0xff918786), sound: 'sounds/note3.wav'),
+  ContainerModel(color: Color(0xffbdac9c), sound: 'sounds/note4.wav'),
+  ContainerModel(color: Color(0xffb8b4b6), sound: 'sounds/note5.wav'),
+  ContainerModel(color: Color(0xff80898b), sound: 'sounds/note6.wav'),
+  ContainerModel(color: Color(0xffe1d5c7), sound: 'sounds/note7.wav'),
+];
--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,23 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:tunes_player_app/Constant/ContainerColors.dart';
 import 'package:tunes_player_app/Widets/ItemWidget.dart';
 import 'package:tunes_player_app/models/ContainerModel.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
-  List<ContainerModel> container = [
-    ContainerModel(color: Colors.red, sound: 'sounds/note1.wav'),
-    ContainerModel(color: Colors.pinkAccent, sound: 'sounds/note2.wav'),
-    ContainerModel(color: Colors.pink, sound: 'sounds/note3.wav'),
-    ContainerModel(color: Colors.orange, sound: 'sounds/note4.wav'),
-    ContainerModel(color: Colors.deepOrange, sound: 'sounds/note5.wav'),
-    ContainerModel(color: Colors.deepOrangeAccent, sound: 'sounds/note6.wav'),
-    ContainerModel(color: Colors.yellowAccent, sound: 'sounds/note7.wav'),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          backgroundColor: Color(0xff4f697a),
           elevation: 0.0,
           //to make center without wrap with center in appBar
           centerTitle: true,
